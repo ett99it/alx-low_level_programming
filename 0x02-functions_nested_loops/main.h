0e114979b7bf61ad9c9a712b82b94d3890012a7e
Salah Eddine <gosudosu0@gmail.com>
2023-02-21 16:52:21 +0100

    0x02-C

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,5 @@
-#ifndef _ALX_h_
-#define _ALX_h_
+#ifndef _main_h_
+#define _main_h_
 
 int _putchar(char);
 void print_alphabet(void);
@@ -13,6 +13,6 @@ void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
-void print_times_table(int n);
+
 
 #endif
